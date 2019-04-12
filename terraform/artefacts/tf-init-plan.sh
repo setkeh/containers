cdb742f9dc92af9badd5cfef0ea636174b8c1813
James Griffis <jgriffis@assemblypayments.com>
2019-04-12 12:53:04 +1000

    adding a script for auth and tf init

---
+++ terraform/artefacts/tf-init-plan.sh
@@ -0,0 +1,6 @@
+export creds=$(aws sts assume-role --role-arn ${ASSUMED_ROLE} --role-session-name AWSCLI-Session)
+export AWS_ACCESS_KEY_ID=$(echo $creds| jq -r ".Credentials | .AccessKeyId")
+export AWS_SECRET_ACCESS_KEY=$(echo $creds| jq -r ".Credentials | .SecretAccessKey")
+export AWS_SESSION_TOKEN=$(echo $creds| jq -r ".Credentials | .SessionToken")
+terraform init /work/nonprod && \
+terraform plan -o /work/out/plan-${BUILDKITE_BUILD_NUMBER} ${WORK_DIR}/nonprod
