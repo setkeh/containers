bb9e4329d1ca00570b830d1447832d25fa0dc198
James Griffis <jgriffis@assemblypayments.com>
2019-04-12 14:21:43 +1000

    pushing up param change to terraform plan

--- terraform/artefacts/tf-init-plan.sh
+++ terraform/artefacts/tf-init-plan.sh
@@ -5,4 +5,4 @@ export AWS_ACCESS_KEY_ID=$(echo $creds| jq -r ".Credentials | .AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(echo $creds| jq -r ".Credentials | .SecretAccessKey")
 export AWS_SESSION_TOKEN=$(echo $creds| jq -r ".Credentials | .SessionToken")
 terraform init /work/nonprod && \
-terraform plan -o /work/out/plan-${BUILDKITE_BUILD_NUMBER} ${WORK_DIR}/nonprod
+terraform plan -out=/work/out/plan-${BUILDKITE_BUILD_NUMBER} ${WORK_DIR}/nonprod
